26dd2bb0e6f0bdd323617a0ed3323181575aa25c
Borisz <vorosborisz@gmail.com>
2019-10-14 00:14:42 +0200

    treatment textareas added autocomplete from taxonomy vocabularies

--- sites/all/modules/motor/js/motor_autocomplete.js
+++ sites/all/modules/motor/js/motor_autocomplete.js
@@ -3,9 +3,14 @@
 		attach: function (context, settings) {
 
 
-    		treatmentAnamnesisTerms = Drupal.settings.treatments_anamnesis;
-    		treatmentComplementaryTerms = Drupal.settings.treatments_complementary;
-    		treatmentNotesTerms = Drupal.settings.treatments_notes;
+    		treatments_anamnesis = Drupal.settings.treatments_anamnesis;
+    		treatments_present_state = Drupal.settings.treatments_present_state;
+    		treatments_diagnosis = Drupal.settings.treatments_diagnosis;
+    		treatments_complementary = Drupal.settings.treatments_complementary;
+    		treatments_notes = Drupal.settings.treatments_notes;
+    		treatments_operation = Drupal.settings.treatments_operation;
+
+    		console.log(Drupal.settings);
 
     		function split( val ) {
 		      return val.split( /,\s*/ );
@@ -13,6 +18,7 @@
 		    function extractLast( term ) {
 		      return split( term ).pop();
 		    }
+
     
 			$('.field-name-field-anamnesis textarea')
 			.keydown(function( event ) {
@@ -25,7 +31,31 @@
 				minLength: 0,
 				source: function( request, response ) {
 				  response( $.ui.autocomplete.filter(
-				    treatmentAnamnesisTerms, extractLast( request.term ) ) );
+				    treatments_anamnesis, extractLast( request.term ) ) );
+				},
+				focus: function() { return false; },
+				select: function( event, ui ) {
+				  var terms = split( this.value );
+				  terms.pop();
+				  terms.push( ui.item.value );
+				  terms.push( "" );
+				  this.value = terms.join( ", " );
+				  return false;
+				}
+			}).css('border', '1px solid red');
+
+			$('.field-name-field-present-state textarea')
+			.keydown(function( event ) {
+				if ( event.keyCode === $.ui.keyCode.TAB &&
+				    $( this ).autocomplete( "instance" ).menu.active ) {
+				  event.preventDefault();
+				}
+			})
+			.autocomplete({
+				minLength: 0,
+				source: function( request, response ) {
+				  response( $.ui.autocomplete.filter(
+				    treatments_present_state, extractLast( request.term ) ) );
 				},
 				focus: function() { return false; },
 				select: function( event, ui ) {
@@ -40,6 +70,35 @@
 
 
 
+
+			$('.field-name-field-diagnosis textarea')
+			.keydown(function( event ) {
+				if ( event.keyCode === $.ui.keyCode.TAB &&
+				    $( this ).autocomplete( "instance" ).menu.active ) {
+				  event.preventDefault();
+				}
+			})
+			.autocomplete({
+				minLength: 0,
+				source: function( request, response ) {
+				  response( $.ui.autocomplete.filter(
+				    treatments_diagnosis, extractLast( request.term ) ) );
+				},
+				focus: function() { return false; },
+				select: function( event, ui ) {
+				  var terms = split( this.value );
+				  terms.pop();
+				  terms.push( ui.item.value );
+				  terms.push( "" );
+				  this.value = terms.join( ", " );
+				  return false;
+				}
+			}).css('border', '1px solid red');
+
+
+
+
+
 			$('.field-name-field-complementary textarea')
 			.keydown(function( event ) {
 				if ( event.keyCode === $.ui.keyCode.TAB &&
@@ -51,7 +110,7 @@
 				minLength: 0,
 				source: function( request, response ) {
 				  response( $.ui.autocomplete.filter(
-				    treatmentComplementaryTerms, extractLast( request.term ) ) );
+				    treatments_complementary, extractLast( request.term ) ) );
 				},
 				focus: function() { return false; },
 				select: function( event, ui ) {
@@ -87,6 +146,31 @@
 				  return false;
 				}
 			}).css('border', '1px solid red');
+
+			$('.field-name-field-operation textarea')
+			.keydown(function( event ) {
+				if ( event.keyCode === $.ui.keyCode.TAB &&
+				    $( this ).autocomplete( "instance" ).menu.active ) {
+				  event.preventDefault();
+				}
+			})
+			.autocomplete({
+				minLength: 0,
+				source: function( request, response ) {
+				  response( $.ui.autocomplete.filter(
+				    treatments_operation, extractLast( request.term ) ) );
+				},
+				focus: function() { return false; },
+				select: function( event, ui ) {
+				  var terms = split( this.value );
+				  terms.pop();
+				  terms.push( ui.item.value );
+				  terms.push( "" );
+				  this.value = terms.join( ", " );
+				  return false;
+				}
+			}).css('border', '1px solid red');
+
 		}
 	}
 }(jQuery));
